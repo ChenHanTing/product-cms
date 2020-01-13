48ba92c956ab08c406a81a54ca2fddc56f81af22
Han <han@jvdiamondtech.com>
2020-01-13 17:41:37 +0800

    add devise, mysql, and welcome page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'chenruru', password: 'as789123456',
+            email: 'wss200086@gmail.com', admin: true)
+User.create(username: 'chenhanhan', password: 'as789123456',
+            email: 'han@jvdiamondtech.com', admin: true)
